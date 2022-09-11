cc72ac01c26d52a772c89a169f783a2ca97782e1
Andrey5353 <102484864+Andrey5353@users.noreply.github.com>
2022-09-11 16:51:11 +0000

    Deploying to gh-pages from @ Andrey5353/goit-js-hw-09@95cca2d6b3671bf388e82a0906a591963d80fe69 🚀

---
+++ 01-color-switcher.8c354c12.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");var n=null;t.addEventListener("click",(function(){t.setAttribute("disabled",!0),n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled",!0),clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.8c354c12.js.map
